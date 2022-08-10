6f5bfad2cd0791c9807f42747f7e6841931dcf56
iaia <iaia72160@gmail.com>
2022-08-10 22:23:20 +0900

    Fix

--- core/src/androidMain/kotlin/dev/iaiabot/furuhuru/di/CoreAndroidModule.kt
+++ core/src/androidMain/kotlin/dev/iaiabot/furuhuru/di/CoreAndroidModule.kt
@@ -1,11 +1,12 @@
 package dev.iaiabot.furuhuru.di
 
 import android.util.LruCache
+import dev.iaiabot.furuhuru.datasource.local.ScreenshotDataSource
 import dev.iaiabot.furuhuru.datasource.local.ScreenshotDataSourceImpl
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val coreAndroidModule = module {
     single(named("ScreenShotCache")) { LruCache<String, String>(1) }
-    single<ScreenshotDataSourceImpl> { ScreenshotDataSourceImpl(get(named("ScreenShotCache"))) }
+    single<ScreenshotDataSource> { ScreenshotDataSourceImpl(get(named("ScreenShotCache"))) }
 }
