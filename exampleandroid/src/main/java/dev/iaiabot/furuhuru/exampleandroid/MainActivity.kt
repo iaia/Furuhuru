09a313a39c3278b80a7a198f63812e1aacf0c58a
iaia <iaia72160@gmail.com>
2022-08-11 00:21:22 +0900

    Fix

--- exampleandroid/src/main/java/dev/iaiabot/furuhuru/exampleandroid/MainActivity.kt
+++ exampleandroid/src/main/java/dev/iaiabot/furuhuru/exampleandroid/MainActivity.kt
@@ -3,7 +3,10 @@ package dev.iaiabot.furuhuru.exampleandroid
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -15,15 +18,21 @@ class MainActivity : AppCompatActivity() {
 
         setContent {
             MaterialTheme {
-                val navController = rememberNavController()
-                val applicationInfo = ApplicationInfo("Furuhuru_example", "12.345")
 
-                NavHost(navController = navController, startDestination = "first") {
-                    composable("first") {
-                        FirstScreen(navController, applicationInfo)
-                    }
-                    composable("second") {
-                        SecondScreen(navController, applicationInfo)
+                Surface(
+                    color = MaterialTheme.colors.background,
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    val navController = rememberNavController()
+                    val applicationInfo = ApplicationInfo("Furuhuru_example", "12.345")
+
+                    NavHost(navController = navController, startDestination = "first") {
+                        composable("first") {
+                            FirstScreen(navController, applicationInfo)
+                        }
+                        composable("second") {
+                            SecondScreen(navController, applicationInfo)
+                        }
                     }
                 }
             }
