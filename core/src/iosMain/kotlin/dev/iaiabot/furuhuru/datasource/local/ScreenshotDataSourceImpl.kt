4bd77d34a1296cf8d0702b97056b8606ce350391
iaia <iaia72160@gmail.com>
2022-08-18 20:00:16 +0900

    Implement ios screenshot data source

--- core/src/iosMain/kotlin/dev/iaiabot/furuhuru/datasource/local/ScreenshotDataSourceImpl.kt
+++ core/src/iosMain/kotlin/dev/iaiabot/furuhuru/datasource/local/ScreenshotDataSourceImpl.kt
@@ -1,21 +1,43 @@
 package dev.iaiabot.furuhuru.datasource.local
 
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import platform.Foundation.NSCache
+import platform.objc.objc_sync_enter
+import platform.objc.objc_sync_exit
 
-internal actual class ScreenshotDataSourceImpl : ScreenshotDataSource {
-    override val screenShotFlow: Flow<String?>
-        get() = TODO("Not yet implemented")
+internal actual class ScreenshotDataSourceImpl(
+    private val cache: NSCache,
+) : ScreenshotDataSource {
+    companion object {
+        private const val SCREENSHOT_KEY = "screenshot"
+    }
+
+    override val screenShotFlow = MutableStateFlow(null)
 
     override suspend fun save(fileStr: String) {
-        TODO("Not yet implemented")
+        synchronized(cache) {
+            cache.setObject(SCREENSHOT_KEY, fileStr)
+        }
     }
 
     override suspend fun remove() {
-        TODO("Not yet implemented")
+        synchronized(cache) {
+            cache.removeObjectForKey(SCREENSHOT_KEY)
+            screenShotFlow.tryEmit(null)
+        }
     }
 
     override suspend fun load(): String? {
-        TODO("Not yet implemented")
+        var screenshotStr: String? = null
+        synchronized(cache) {
+            screenshotStr = cache.objectForKey(SCREENSHOT_KEY) as? String
+        }
+        return screenshotStr
     }
 
+    private fun synchronized(obj: Any, exec: () -> Unit) {
+        objc_sync_enter(obj)
+        exec.invoke()
+        objc_sync_exit(obj)
+    }
 }
