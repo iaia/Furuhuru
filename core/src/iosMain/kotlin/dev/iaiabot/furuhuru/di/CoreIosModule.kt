67b1df463894cc2b9b2a5d73830f64cc942f38ba
iaia <iaia72160@gmail.com>
2022-08-18 20:06:42 +0900

    Impl ios module

--- core/src/iosMain/kotlin/dev/iaiabot/furuhuru/di/CoreIosModule.kt
+++ core/src/iosMain/kotlin/dev/iaiabot/furuhuru/di/CoreIosModule.kt
@@ -1,10 +1,16 @@
 package dev.iaiabot.furuhuru.di
 
+import dev.iaiabot.furuhuru.datasource.local.ScreenshotDataSource
+import dev.iaiabot.furuhuru.datasource.local.ScreenshotDataSourceImpl
+import dev.iaiabot.furuhuru.datasource.local.UserDataSource
+import dev.iaiabot.furuhuru.datasource.local.UserDataSourceImpl
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
+import platform.Foundation.NSCache
 
-val coreAndroidModule = module {
-    // single(named("ScreenShotCache")) { LruCache<String, String>(1) }
-    // single<ScreenshotDataSource> { ScreenshotDataSourceImpl(get(named("ScreenShotCache"))) }
-    // single<UserDataSource> { UserDataSourceImpl(get()) }
+val coreIosModule = module {
+    single(named("ScreenShotCache")) { NSCache() }
+
+    single<ScreenshotDataSource> { ScreenshotDataSourceImpl(get(named("ScreenShotCache"))) }
+    single<UserDataSource> { UserDataSourceImpl(get()) }
 }
-
