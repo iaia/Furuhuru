e2b0e930399d444766e09738f0cc2237ab425de0
iaia <iaia72160@gmail.com>
2022-08-18 20:45:17 +0900

    test

--- iosFuruhuru/iosFuruhuru/ContentView.swift
+++ iosFuruhuru/iosFuruhuru/ContentView.swift
@@ -2,15 +2,24 @@ import SwiftUI
 import core
 
 struct ContentView: View {
-	let greet = Greeting().greeting()
-
-	var body: some View {
-		Text(greet)
-	}
+    
+    let applicationInfo = ApplicationInfo("furuhuru-ios-example", "1.2.3")
+    
+    let greet = IssueBodyBuilder.build(
+        "username",
+        "usersBody"
+        "imageUrl"
+        "fileUrl",
+        applicationInfo
+    )
+    
+    var body: some View {
+        Text(greet)
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
-	static var previews: some View {
-		ContentView()
-	}
+    static var previews: some View {
+        ContentView()
+    }
 }
